a7217929fedd88765ac4d2b022d706a9c46af986
cashpeakapp-boop <cashpeak.app@gmail.com>
2026-08-09 20:56:27 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 void main() {
   runApp(const CashPeakApp());
 }
